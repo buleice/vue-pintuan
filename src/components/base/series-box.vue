942da63bba6beedb4ed8b8169e751ec82ecebc4e
dylan <dylan@youban.org>
2018-11-02 21:24:58 +0800

    隐藏入口

--- src/components/base/series-box.vue
+++ src/components/base/series-box.vue
@@ -1,6 +1,6 @@
 <template lang="html">
-  <div class="course-contatiner">
-    <ul v-if="series">
+  <div class="course-contatiner" v-if="series">
+    <ul>
       <li v-for='(serie,index) in series' :key="index">
         <a :href="'/purchase/series?id='+serie.id"><img :src="serie.img" alt="图片"> </a>
       </li>
